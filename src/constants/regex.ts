3153b0d998b4875fc44a3b87332a4c8c311919f0
Dinozavrik35 <dinozavrik35@mail.ru>
2024-02-12 00:07:36 +0300

    add constants for regex

---
+++ src/constants/regex.ts
@@ -0,0 +1,5 @@
+export const phoneRegex = new RegExp(/^\+7\s\d{3}\s\d{3}-\d{2}-\d{2}$/);
+export const zipcodeRegex = new RegExp(/^\d+-?\d*$/);
+export const noLettersRegex = new RegExp(/^[^A-Z]*$/gi);
+export const anySybmolRegex = new RegExp(/\S*/g);
+export const filterValueRegex = new RegExp(/[_()+.-]/g);
