a4478d84be05699ce96488d9c35e1fdeefc7bff9
Dinozavrik35 <dinozavrik35@mail.ru>
2024-02-12 20:19:38 +0300

    update phone regex

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,5 +1,5 @@
 export const phoneRegex = new RegExp(/^\+7\s\d{3}\s\d{3}-\d{2}-\d{2}$/);
 export const zipcodeRegex = new RegExp(/^\d+-?\d*$/);
-export const noLettersRegex = new RegExp(/^[^A-Z]*$/gi);
+export const noLettersRegex = new RegExp(/^[^\p{L}]*$/gui);
 export const anySybmolRegex = new RegExp(/\S*/g);
 export const filterValueRegex = new RegExp(/[_()+.-]/g);
